f2a584686e2927bd39ea3547259913967e922fe1
Oliver G <o.gough@ucl.ac.uk>
2025-03-12 16:30:15 +0000

    Create cluster runscript

---
+++ runscript.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Request 4 hrs of wallclock time (format hours:minutes:seconds).
+#$ -l h_rt=4:00:00
+
+# Request 8 gigabyte of RAM
+#$ -l mem=8G
+
+
+# Set the name of the job.
+#$ -N cd7_exon_align
+
+# Request 12 cores.
+#$ -pe smp 12
+
+### Set Working Directory - CHANGE THIS EACH TIME! ###
+#$ -wd /home/sejjojg/Scratch/workspace/exon_smk_test/
+
+# Load modules
+
+. $MODULESHOME/init/bash
+
+module load python/miniconda3/4.10.3
+
+source $UCL_CONDA_PATH/etc/profile.d/conda.sh
+
+# Establish snakemake env
+
+conda init bash
+
+conda env create -f ./workflow/envs/snakemake.yml
+
+conda activate snakemake
+
+# Run
+
+snakemake --sdm conda
